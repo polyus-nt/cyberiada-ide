c71e2036972a459a58bb76cd1038206d40474ec3
Mikhail Chekan <chekoopa@mail.ru>
2025-02-05 15:50:18 +0800

    Правки для выпуска Cyberiada IDE v0.5.0

--- src/main/version.ts
+++ src/main/version.ts
@@ -1,5 +1,5 @@
-export const defaultRemoteDocHost = 'https://lapki-doc.polyus-nt.ru/v/0.4.0/';
-export const defaultCompilerHost = 'lapki.polyus-nt.ru';
+export const defaultRemoteDocHost = 'https://ide-doc.polyus-nt.ru/cyberiada/0.4.0/';
+export const defaultCompilerHost = 'ide.polyus-nt.ru';
 export const defaultCompilerPort = 8084;
-export const autoUpdateHost = // undefined;
-  'https://api.github.com/repos/kruzhok-team/lapki-client/releases/latest';
+export const autoUpdateHost =
+  'https://api.github.com/repos/polyus-nt/cyberiada-ide/releases/latest';
